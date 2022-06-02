ac12e91a6b95381acdd1ebe87dd8ae9e9c669363
grisonrf <grisonrf@gmail.com>
2022-06-02 14:00:19 -0600

    halfway through step 3

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -6,7 +6,56 @@ const tradingCardData = [
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg',
   },
+  {
+    name: "Float",
+    skill: "baking pretzels",
+    imgUrl: "/static/img/float.jpg",
+    cardId: 2,
+  },
+  {
+    name: "Llambda",
+    skill: "knitting scarves",
+    imgUrl: "/static/img/llambda.jpg",
+    cardId: 3,
+  },
+  {
+    name: "Off-By-One",
+    skill: "climbing mountains",
+    imgUrl: "/static/img/off-by-one.jpeg",
+    cardId: 4,
+  },
+  {
+    name: "Seed.py",
+    skill: "making curry dishes",
+    imgUrl: "/static/img/seedpy.jpeg",
+    cardId: 5,
+  },
+  {
+    name: "Polymorphism",
+    skill: "costumes",
+    imgUrl: "/static/img/polymorphism.jpeg",
+    cardId: 6,
+  },
+  {
+    name: "Short Stack Overflow",
+    skill: "ocean animal trivia",
+    imgUrl: "/static/img/shortstack-overflow.jpeg",
+    cardId: 7,
+  },
+  {
+    name: "Merge",
+    skill: "bullet journaling",
+    imgUrl: "/static/img/merge.png",
+    cardId: 8,
+  },
 ];
+
+
+
+// for (i of tradingCardData) {
+//   console.log(i.name, i.skill)
+// }
+
 
 function TradingCard(props) {
   return (
